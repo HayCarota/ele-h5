96a4436c6734162ea07bca554aa1942fac5b1658
HayCarota <1914810765@qq.com>
2024-01-30 00:20:51 +0800

    feat: 安装使用vant-ui；vue-router实现页面路由；

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import TabsView from '@/views/tabs/TabsView.vue'
+import HomeViewVue from '@/views/tabs/home/HomeView.vue'
+import OrderViewVue from '@/views/tabs/order/OrderView.vue'
+import MeViewVue from '@/views/tabs/me/MeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), // 设置history，就不会带哈希#
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/home'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/tabs',
+      name: 'tabs',
+      component: TabsView,
+      children: [
+        {name: 'home', path: '/home', component: HomeViewVue},
+        {name: 'order', path: '/order', component: OrderViewVue},
+        {name: 'me', path: '/me', component: MeViewVue}
+      ]
     }
   ]
 })
 
+// 创建router实例，并导出
 export default router
---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterView, useRoute, useRouter } from 'vue-router';
+import { Tabbar, TabbarItem } from 'vant'
+import { ref, watch } from 'vue'
+
+const route = useRoute(); // 1、当前路由状态hook
+const router = useRouter(); // 2、获取路由实例hook
+//记录当前选择的 tab
+const active = ref(route.name as string)
+
+//3、监听路由变化，更新选中的 tab
+watch(active, (newActive) => {
+    router.push({ name: newActive })
+})
+
+</script>
+
+<template>
+  <!-- <div class="center">{{ active }}</div> -->
+  <!-- 内容渲染地方 路由出口 -->
+  <RouterView />
+  <Tabbar v-model="active">
+    <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+    <TabbarItem name="order" icon="bars">订单</TabbarItem>
+    <TabbarItem name="me" icon="contact">我的</TabbarItem>
+  </Tabbar>
+</template>
+
+<style>
+.center {
+  text-align: center;
+}
+</style>
